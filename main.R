b846edbbeef8114c32c8b6be7a26a232bc70698b
Corvin Idler <corvin.idler@gmail.com>
2024-07-24 14:32:21 +1100

    Implemented most of the input validation and embedding of script into markdown file

--- main.R
+++ main.R
@@ -1,3 +1,63 @@
 shapirowilk<-function(x, AD=FALSE){
+  datname <- deparse(substitute(x))
   
-}
+  ##Error handling
+  #check if input parameter is empty
+  tryCatch(
+    {
+      stopifnot(!missing(x))
+    }, 
+    error=function(e){
+      message('It appears that no input data set was specified. Make sure to pass a numeric vector to the function.')
+      stop(e)
+    }
+  )
+
+  #check if input parameter is a numeric vector
+  tryCatch(
+    {
+      stopifnot(is.numeric(x))
+    }, 
+    error=function(e){
+      message(paste0('It appears that the input is not a numeric vector but instead of class ',class(x)))
+      stop(e)
+    }
+  )
+  
+  #check if NAs are present and filter
+  tryCatch(
+    {
+      stopifnot(!anyNA(x))
+    }, 
+    error=function(e){
+      message('It appears that the input vector contains NA values. They will be filterd out.')
+    }
+  )
+  
+  #check if inf are present and filter
+  tryCatch(
+    {
+      stopifnot(all(is.finite(na.omit(x))))
+    }, 
+    error=function(e){
+      message('It appears that the input vector contains INF values. They will be filterd out.')
+      filteredx<-na.omit(x)
+    }
+  )
+
+  #check if vector has the right length
+  tryCatch(
+    {
+      n<-length(x[!is.na(x) & !is.infinite(x)])
+      stopifnot(n >=3 && n <=5000)
+    }, 
+    error=function(e){
+      message('It appears that the input vector is shorter then 3 or longer than 5000. That might be due to filtering out INF and NA values. Make sure to stay within these limits.')
+      stop(e)
+    }
+  )
+  
+ shapiro.test(x[!is.na(x) & !is.infinite(x)])
+}  
+
+ 
